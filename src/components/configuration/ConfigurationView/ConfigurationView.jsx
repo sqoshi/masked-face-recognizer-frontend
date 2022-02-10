75a80fd3fc60397ada5f6b6eb5c47f59b916a9d5
sqoshi <!piter68>
2022-02-10 23:43:00 +0100

    configruatio nview almost finished not styled

--- src/components/configuration/ConfigurationView/ConfigurationView.jsx
+++ src/components/configuration/ConfigurationView/ConfigurationView.jsx
@@ -2,23 +2,77 @@ import {Component} from "react";
 import Collapsible from "react-collapsible";
 
 
+function createCollapsibleHeader(text) {
+    return <p style={{cursor: "pointer", backgroundColor: "green", borderRadius: "10px"}}>
+        {text}
+    </p>
+}
+
+function generateInputs(dictionary, header) {
+    return <div>
+        <p>{header}</p>
+        <ul>
+            {Object.keys(dictionary).map((item) => {
+                if (typeof dictionary[item] != "object")
+                    return <li>
+                        <label>{item}:
+                            <input value={dictionary[item]}/>
+                        </label>
+                    </li>
+                else {
+                    return generateInputs(dictionary[item], item)
+                }
+            })}
+        </ul>
+    </div>
+}
+
 class ConfigurationView extends Component {
     state = {configurationList: []}
 
     addCollapsibleOnBtnClick = () => {
         this.setState({
-            configurationList: this.state.configurationList.concat(this.createCollapsible())
+            configurationList: this.state.configurationList.concat(this.createEmptyConfiguration())
         })
     };
 
-    createCollapsible() {
+    createEmptyConfiguration = () => {
+        return {
+            "dataset": "celeba",
+            "name": "",
+            "split_ratio": 0.6,
+            "piq": 1,
+            "equal_piqs": true,
+            "identities_limit": 40,
+            "landmarks_detection": false,
+            "train_set_mods": {
+                "mask_ratio": 0.6,
+                "inplace": false,
+                "mask": 0
+            },
+            "test_set_mods": {
+                "mask_ratio": 0.6,
+                "inplace": false,
+                "mask": 0
+            },
+            "svm_config": {
+                "C": 1.0,
+                "kernel": "linear",
+                "degree": 5,
+                "probability": true,
+                "random_state": true,
+            }
+
+        }
+    }
+
+
+    createCollapsible(configuration) {
         return <Collapsible
             key={this.state.configurationList.length}
-            trigger={`Configuration ` + this.state.configurationList.length}
+            trigger={createCollapsibleHeader(`Configuration #` + this.state.configurationList.length)}
         >
-            <p>Configure SVM</p>
-            <p>Train Set Modification</p>
-            <p>Test Set Modification</p>
+            {generateInputs(configuration, "Default config")}
         </Collapsible>
     }
 
@@ -26,11 +80,10 @@ class ConfigurationView extends Component {
     render() {
         return (
             <div style={{top: "10px"}}>
-                <button onClick={this.addCollapsibleOnBtnClick}>Add input</button>
-                {/*{createCollapsible("Configure Learning Procedure")}*/}
-                {/*{createCollapsible("Configure Learning Procedure")}*/}
-                {console.log(this.state.configurationList)}
-                {this.state.configurationList}
+                <button onClick={this.addCollapsibleOnBtnClick} style={{padding: "30px"}}>Add input</button>
+                {this.state.configurationList.map((item) => {
+                    return this.createCollapsible(item)
+                })}
             </div>
         )
     }
